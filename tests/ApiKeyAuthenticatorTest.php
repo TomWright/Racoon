2eff6a68eac98f2e94e20bffa81b1be008d98bc9
Tom Wright <contact@tomwright.me>
2016-07-12 17:20:25 +0100

    Add some more unit tests.

--- tests/ApiKeyAuthenticatorTest.php
+++ tests/ApiKeyAuthenticatorTest.php
@@ -2,6 +2,7 @@
 
 use Racoon\Api\Auth\ApiKeyAuthenticator;
 use Racoon\Api\Exception\AuthenticationException;
+use Racoon\Api\Request;
 use Racoon\Api\Test\TestBase;
 
 class ApiKeyAuthenticatorTest extends TestBase
@@ -14,6 +15,87 @@ class ApiKeyAuthenticatorTest extends TestBase
         $auth->addValidApiKey('valid');
         $app->setAuthenticator($auth);
         return $app;
+    }
+
+
+    public function testNoApiKeyProvided()
+    {
+        $request = new Request();
+        $requestData = new stdClass();
+        $auth = new ApiKeyAuthenticator();
+
+        $auth->setValidApiKeys(['valid1', 'valid2']);
+
+        $valid = true;
+        try {
+            $request->setRequest($requestData);
+            $auth->authenticate($request);
+        } catch (AuthenticationException $e) {
+            $valid = false;
+        }
+        $this->assertFalse($valid);
+    }
+
+
+    public function testAuthenticatorWorks()
+    {
+        $request = new Request();
+        $requestData = new stdClass();
+        $auth = new ApiKeyAuthenticator();
+
+        $auth->setValidApiKeys(['valid1', 'valid2']);
+        
+        $valid = true;
+        try {
+            $requestData->api_key = 'asdasdasd';
+            $request->setRequest($requestData);
+            $auth->authenticate($request);
+        } catch (AuthenticationException $e) {
+            $valid = false;
+        }
+        $this->assertFalse($valid);
+
+        $valid = true;
+        try {
+            $requestData->api_key = 'valid1';
+            $request->setRequest($requestData);
+            $auth->authenticate($request);
+        } catch (AuthenticationException $e) {
+            $valid = false;
+        }
+        $this->assertTrue($valid);
+
+        $valid = true;
+        try {
+            $requestData->api_key = 'valid2';
+            $request->setRequest($requestData);
+            $auth->authenticate($request);
+        } catch (AuthenticationException $e) {
+            $valid = false;
+        }
+        $this->assertTrue($valid);
+
+        $valid = true;
+        try {
+            $requestData->api_key = 'valid3';
+            $request->setRequest($requestData);
+            $auth->authenticate($request);
+        } catch (AuthenticationException $e) {
+            $valid = false;
+        }
+        $this->assertFalse($valid);
+
+        $auth->addValidApiKey('valid3');
+
+        $valid = true;
+        try {
+            $requestData->api_key = 'valid3';
+            $request->setRequest($requestData);
+            $auth->authenticate($request);
+        } catch (AuthenticationException $e) {
+            $valid = false;
+        }
+        $this->assertTrue($valid);
     }
 
 
