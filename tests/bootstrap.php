4776a4c738b7e3b3eb5ce4f9bb530f02e361305a
Tom Wright <contact@tomwright.me>
2016-07-12 16:27:27 +0100

    Use Composer to autoload the Test classes.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,6 @@ define('ENVIRONMENT', 'phpunit');
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-define('TEST_SRC_PATH', __DIR__ . '/../tests/src');
-
-require_once TEST_SRC_PATH . '/TestApp.php';
-require_once TEST_SRC_PATH . '/TestBase.php';
-require_once TEST_SRC_PATH . '/TestController.php';
+$al = new \Composer\Autoload\ClassLoader();
+$al->addPsr4('Racoon\\Api\\Test\\', __DIR__ . '/../tests/src');
+$al->register();
